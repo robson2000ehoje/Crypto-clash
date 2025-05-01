70b8105addbf1d8ed702d43ad98b94644f23797f
robson2000ehoje <robsono554@gmail.com>
2025-04-30 22:16:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("start-game").addEventListener("click", function() {
+  alert("Jogo iniciado! Selecione sua facção...");
+  // Aqui podemos redirecionar para a tela de seleção de facção
+});
+
+document.getElementById("rules").addEventListener("click", function() {
+  alert("Regras do jogo: Se torne o império cripto mais poderoso!");
+  // Exibir regras detalhadas em um modal ou nova página
+});
